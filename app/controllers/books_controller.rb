f69b08204bf59afd29e8ebcc9437e11de9d7be42
Zak Stone <zrlstone@gmail.com>
2022-09-19 17:52:51 +0100

    Add create book action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,61 @@
 class BooksController < ApplicationController
+  def create
+    client = Notion::Client.new
+    properties = {
+      'Title': {
+        'title': [
+          {
+            'text': {
+              'content': 'Tuscan Kale'
+            }
+          }
+        ]
+      },
+      'Author': {
+        "rich_text": [
+          {
+            "type": "text",
+            "text": {
+              "content": "Some more text with "
+            }
+          },
+        ]
+      },
+      "Category": {
+        "select": {
+          "name": "Book"
+        }
+      },
+      'Full Title': {
+        "rich_text": [
+          {
+            "type": "text",
+            "text": {
+              "content": "Tuscan Kale: The Adventures of"
+            }
+          },
+        ]
+      },
+      "Last Highlighted": {
+        "date": {
+          "start": "2021-05-11T11:00:00.000"
+        }
+      },
+      "Last Synced": {
+        "date": {
+          "start": "2021-05-11T11:00:00.000"
+        }
+      },
+      "URL": {
+        "url": "https://notion.so/notiondevs"
+      },
+      "Highlights": {
+        "number": 1234
+      }
+    }
+    client.create_page(
+       parent: { database_id: ENV['NOTION_DATABASE_ID']},
+       properties: properties
+    )
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+    client = Notion::Client.new
 end
